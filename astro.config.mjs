4924002ccb34fcb4f1f6688174ee7d0687bec804
Bryan Villanueva <vbryan0803@gmail.com>
2025-08-19 22:05:24 +1000

    Primeros modulos y Hero section terminada

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
@@ -12,6 +9,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [react(), sitemap()]
 });
